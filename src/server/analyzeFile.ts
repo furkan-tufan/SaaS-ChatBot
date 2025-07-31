a89ea6f11873ea3fdcb250b1171ebb7e7dd2c701
Furkan Tufan <furkanismettufan@gmail.com>
2025-07-31 14:48:53 +0300

    fix

--- src/server/analyzeFile.ts
+++ src/server/analyzeFile.ts
@@ -1,6 +1,11 @@
-import { config } from 'wasp/client'
+import type { AnalyzeFile } from "wasp/server/api";
+import { Request, Response } from "express";
 
-const response = await fetch(
-  `${config.apiUrl}/analyze`, 
-  { method: "POST", body: formData }
-)
+// Wasp, burada 3. argüman olarak bir context objesi (user, entities vb.) geçiriyor
+export const analyzeFile: AnalyzeFile = (
+  req: Request,
+  res: Response,
+  context
+) => {
+  res.status(200).json({ summary: "Bu bir test özetidir." });
+};
