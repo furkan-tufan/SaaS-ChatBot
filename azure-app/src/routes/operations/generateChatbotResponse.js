e7151f306bd211fd5f975b907e9bfcf0e0ce5fc9
Furkan İsmet Tufan <furkan.tufan@golive.local>
2025-07-31 11:23:22 +0300

    deleted files

--- azure-app/src/routes/operations/generateChatbotResponse.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from '../../middleware/operations.js'
-import generateChatbotResponse from '../../actions/generateChatbotResponse.js'
-
-export default createAction(generateChatbotResponse)
